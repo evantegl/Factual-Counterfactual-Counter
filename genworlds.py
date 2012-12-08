ecbe462982854c71bcc07dd0973991534dfc3969
Malvin Gattinger <illuminatus@ludwig.(none)>
2012-12-08 14:37:58 +0100

    various, empty functions added to updates

--- genworlds.py
+++ genworlds.py
@@ -3,18 +3,18 @@ import itertools
 truthvalues=[0,1]
 
 def worldgen(lang): # generate all possible worlds for some propositions
-    result=[]
-    worldname=-1
-    truthtable = itertools.product(truthvalues, repeat=len(lang))
-    for line in truthtable:
-	worldname=worldname+1
-	world={}
-	world["meta"]={}
-	world["values"]={}
-	world["meta"]["name"]="w_"+str(worldname)
-	world["meta"]["US"]=True
-	world["meta"]["FS"]=True
-	for j in range(len(lang)):
-	    world["values"][lang[j]]=line[j]
-	result.append(world)
-    return result
+	result=[]
+	worldname=-1
+	truthtable = itertools.product(truthvalues, repeat=len(lang))
+	for line in truthtable:
+		worldname=worldname+1
+		world={}
+		world["meta"]={}
+		world["values"]={}
+		world["meta"]["name"]="w_"+str(worldname)
+		world["meta"]["US"]=True
+		world["meta"]["FS"]=True
+		for j in range(len(lang)):
+			world["values"][lang[j]]=line[j]
+		result.append(world)
+	return result
--- truthchecker.py
+++ truthchecker.py
@@ -1,36 +1,36 @@
 def chop(formula): # count brackets to get main connective and return subformulas
-    """
-    INPUT: a formula
-    OUTPUT: result as a triple: (connective, subleft, subright)
-    WHAT IT DOES: Finds the main connective by counting brackets. It returns the two (or in case of negation: one) subformulas and the connective.
-    """
-    min_pcount, pcount = 10000, 0
-    conpos=0
-    pos=0
-    result={}
-    for pos, c in enumerate(formula):
-	if c == '(': pcount += 1
-	if c == ')': pcount -= 1
-	if (c == '~' or c == '&' or c=='|' or c==">"):
-	    if pcount < min_pcount:
-		min_pcount = pcount
-		result["connective"] = c
-		conpos = pos
-    if not conpos==0: # we only have a left subformula if it's not a negation
-	result["subleft"]=formula[1:(conpos-1)]
-    result["subright"]=formula[(conpos+2):-1]
-    return result
+	"""
+	INPUT: a formula
+	OUTPUT: result as a triple: (connective, subleft, subright)
+	WHAT IT DOES: Finds the main connective by counting brackets. It returns the two (or in case of negation: one) subformulas and the connective.
+	"""
+	min_pcount, pcount = 10000, 0
+	conpos=0
+	pos=0
+	result={}
+	for pos, c in enumerate(formula):
+		if c == '(': pcount += 1
+		if c == ')': pcount -= 1
+		if (c == '~' or c == '&' or c=='|' or c==">"):
+			if pcount < min_pcount:
+				min_pcount = pcount
+				result["connective"] = c
+				conpos = pos
+	if not conpos==0: # we only have a left subformula if it's not a negation
+		result["subleft"]=formula[1:(conpos-1)]
+	result["subright"]=formula[(conpos+2):-1]
+	return result
 
 def tiw(world,formula): # check whether a formula is true in world
-    if len(formula)==1: # atomic
-	return world["values"][formula]
-    else:
-	structure=chop(formula)
-	if structure["connective"]=="~": # negation
-	    return not tiw(world,structure["subright"])
-	if structure["connective"]=="&": # conjunction
-	    return ( tiw(world,structure["subleft"]) & tiw(world,structure["subright"]) )
-	if structure["connective"]=="|": # disjunction
-	    return ( tiw(world,structure["subleft"]) | tiw(world,structure["subright"]) )
-	if structure["connective"]==">": # disjunction
-	    return ( tiw(world,structure["subleft"]) <= tiw(world,structure["subright"]) )
+	if len(formula)==1: # atomic
+		return world["values"][formula]
+	else:
+		structure=chop(formula)
+		if structure["connective"]=="~": # negation
+			return not tiw(world,structure["subright"])
+		if structure["connective"]=="&": # conjunction
+			return ( tiw(world,structure["subleft"]) & tiw(world,structure["subright"]) )
+		if structure["connective"]=="|": # disjunction
+			return ( tiw(world,structure["subleft"]) | tiw(world,structure["subright"]) )
+		if structure["connective"]==">": # disjunction
+			return ( tiw(world,structure["subleft"]) <= tiw(world,structure["subright"]) )
