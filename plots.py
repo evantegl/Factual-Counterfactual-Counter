8d7d91d23fffd687e3acac88d68b66acfe08d57d
Evante Garza-Licudine <evantegl@gmail.com>
2012-12-09 18:40:37 +0100

    dummy files showing how to plot data from a file

---
+++ plots.py
@@ -0,0 +1,32 @@
+﻿#!/usr/bin/python
+
+from numpy import *
+from pylab import *
+from matplotlib import rc, rcParams
+
+rc('text',usetex=True)
+rc('font',**{'family':'serif','serif':['Droid Sans']})
+
+# Read in data from an ASCII data table
+data = genfromtxt('plotting_data')
+
+#'data' is a matrix containing the columns and rows from the file
+mass   = data[:,0]  # Python indices are (row,col) as in linalg
+radius = data[:,1]  # Creates arrays for first two columns
+
+print mass
+print radius
+
+# Create a loglog plot of data
+loglog(mass,radius)
+xlabel(r'Mass ($M_{\odot}$)')
+ylabel(r'Radius ($R_{\odot}$)')
+
+# Turn on a grid
+grid(True)
+
+# Save the figure in a separate file
+savefig('read_and_plot_data.png')
+
+# Draw the plot to the screen
+show()
